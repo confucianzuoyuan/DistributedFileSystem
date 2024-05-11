172b07309e57e34698c64d30f5045d869e0676da
confucianzuoyuan <zuoyuantc@126.com>
2024-05-12 01:28:11 +0800

    update

--- Dstore.java
+++ Dstore.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,7 @@ public class Dstore {
     ArrayList<String> filesStored;
     Socket toServer;
     File dir;
-    Hashtable<String, Integer> fileSizes = new Hashtable<>();
+    HashMap<String, Integer> fileSizes = new HashMap<>();
     CountDownLatch wait;
 
     public static void main(String[] args) {
@@ -26,18 +27,9 @@ public class Dstore {
         int timeout = Integer.parseInt(args[2]);
         String fileFolder = args[3];
         ArrayList<String> filesStored = new ArrayList<>();
-        Dstore dstore = new Dstore(port, cport, timeout, fileFolder, filesStored);
+        Dstore ignored = new Dstore(port, cport, timeout, fileFolder, filesStored);
     }
 
-    /**
-     * Creates a Dstore to store files
-     *
-     * @param port        to host on
-     * @param cport       to connect to
-     * @param timeout     duration ms
-     * @param fileFolder  location to store files at
-     * @param filesStored files currently stored
-     */
     public Dstore(int port, int cport, int timeout, String fileFolder, ArrayList<String> filesStored) {
         this.port = port;
         this.cport = cport;
@@ -55,11 +47,7 @@ public class Dstore {
             logger.info("error " + e.getMessage());
         }
 
-        new Thread(new Runnable() {
-            public void run() {
-                ServerComms();
-            }
-        }).start();
+        new Thread(this::ServerComms).start();
 
         try {
             ServerSocket ss = new ServerSocket(port);
@@ -80,29 +68,29 @@ public class Dstore {
                                     String[] splitIn = line.split(" ");
                                     String command = splitIn[0];
                                     switch (command) {
-                                        case "STORE":
-                                        case "REBALANCE_STORE":
+                                        case Protocol.STORE_TOKEN:
+                                        case Protocol.REBALANCE_STORE_TOKEN:
                                             String fileName = splitIn[1];
-                                            int size = Integer.valueOf(splitIn[2]);
+                                            int size = Integer.parseInt(splitIn[2]);
                                             byte[] fileBuffer = new byte[size];
                                             int buflen;
                                             File outputFile = new File(dir, fileName);
                                             FileOutputStream out = new FileOutputStream(outputFile);
                                             InputStream fileInStream = client.getInputStream();
                                             logger.info("filename: " + fileName);
-                                            sendMsg(client, "ACK");
+                                            sendMsg(client, Protocol.ACK_TOKEN);
                                             while ((buflen = fileInStream.read(fileBuffer)) != -1) {
                                                 out.write(fileBuffer, 0, buflen);
                                             }
-                                            if (command.equals("STORE")) {
-                                                sendMsg(toServer, "STORE_ACK " + fileName);
+                                            if (command.equals(Protocol.STORE_TOKEN)) {
+                                                sendMsg(toServer, Protocol.STORE_ACK_TOKEN + " " + fileName);
                                             }
                                             filesStored.add(fileName);
                                             fileSizes.put(fileName, size);
                                             fileInStream.close();
                                             out.close();
                                             break;
-                                        case "LOAD_DATA":
+                                        case Protocol.LOAD_DATA_TOKEN:
                                             if (!filesStored.contains(splitIn[1])) {
                                                 client.close();
                                             }
@@ -127,11 +115,10 @@ public class Dstore {
     }
 
     private void ServerComms() {
-
         try {
             // Sending
             toServer = new Socket(InetAddress.getLocalHost(), cport);
-            sendMsg(toServer, "JOIN " + port);
+            sendMsg(toServer, Protocol.JOIN_TOKEN + " " + port);
 
             // Receiving
             try {
@@ -143,15 +130,14 @@ public class Dstore {
                         while ((line = in.readLine()) != null) {
                             logger.info(line + " received");
                             String[] splitIn = line.split(" ");
-                            if (splitIn[0].equals("LIST")) {
-                                String msgToSend = "LIST";
+                            if (splitIn[0].equals(Protocol.LIST_TOKEN)) {
+                                var msgToSend = new StringBuilder(Protocol.LIST_TOKEN);
                                 for (String file : filesStored) {
-                                    msgToSend += " " + file;
+                                    msgToSend.append(" ").append(file);
                                 }
-                                sendMsg(toServer, msgToSend);
-
+                                sendMsg(toServer, msgToSend.toString());
                                 // REMOVE
-                            } else if (splitIn[0].equals("REMOVE")) {
+                            } else if (splitIn[0].equals(Protocol.REMOVE_TOKEN)) {
                                 String fileName = splitIn[1];
                                 if (filesStored.contains(fileName)) {
                                     File toRemove = new File(dir, fileName);
@@ -159,20 +145,20 @@ public class Dstore {
                                     if (toRemove.delete()) {
                                         filesStored.remove(fileName);
                                         logger.info("File " + fileName + " removed");
-                                        sendMsg(toServer, "REMOVE_ACK " + fileName);
+                                        sendMsg(toServer, Protocol.REMOVE_ACK_TOKEN + " " + fileName);
                                     }
                                 } else {
-                                    sendMsg(toServer, "ERROR_FILE_DOES_NOT_EXIST " + fileName);
+                                    sendMsg(toServer, Protocol.ERROR_FILE_DOES_NOT_EXIST_TOKEN + " " + fileName);
                                     logger.info("File " + fileName + " is not stored");
                                 }
-                            } else if (splitIn[0].equals("REBALANCE")) {
-                                Integer noOfFiles = Integer.valueOf(splitIn[1]);
+                            } else if (splitIn[0].equals(Protocol.REBALANCE_TOKEN)) {
+                                int noOfFiles = Integer.parseInt(splitIn[1]);
                                 Hashtable<String, ArrayList<Integer>> filesToSend = new Hashtable<>();
-                                Integer offset = 2;
+                                int offset = 2;
                                 for (int c = 0; c < noOfFiles; c++) {
                                     String fileName = splitIn[offset];
                                     offset += 1;
-                                    Integer noOfStores = Integer.valueOf(splitIn[offset]);
+                                    int noOfStores = Integer.parseInt(splitIn[offset]);
                                     ArrayList<Integer> dStores = new ArrayList<>();
                                     for (int i = 1; i <= noOfStores; i++) {
                                         offset += 1;
@@ -186,7 +172,7 @@ public class Dstore {
                                     Integer fs = fileSizes.get(f);
                                     for (Integer d : filesToSend.get(f)) {
                                         Socket dSock = new Socket(InetAddress.getLocalHost(), d);
-                                        sendMsg(dSock, "REBALANCE_STORE " + f + " " + fs);
+                                        sendMsg(dSock, Protocol.REBALANCE_STORE_TOKEN + " " + f + " " + fs);
                                         wait = new CountDownLatch(1);
                                         new Thread(new Runnable() {
                                             public void run() {
@@ -198,7 +184,7 @@ public class Dstore {
                                                     while ((line2 = in2.readLine()) != null) {
                                                         logger.info(line2 + " received");
                                                         String[] splitIn2 = line2.split(" ");
-                                                        if (splitIn2[0].equals("ACK")) {
+                                                        if (splitIn2[0].equals(Protocol.ACK_TOKEN)) {
                                                             wait.countDown();
                                                         }
                                                     }
@@ -213,7 +199,7 @@ public class Dstore {
                                         dSock.close();
                                     }
                                 }
-                                Integer noToRemove = Integer.valueOf(splitIn[offset]);
+                                int noToRemove = Integer.parseInt(splitIn[offset]);
                                 offset += 1;
                                 for (int c = 0; c < noToRemove; c++) {
                                     if (filesStored.contains(splitIn[offset])) {
@@ -228,11 +214,11 @@ public class Dstore {
                                     }
                                     offset += 1;
                                 }
-                                sendMsg(toServer, "REBALANCE_COMPLETE");
+                                sendMsg(toServer, Protocol.REMOVE_COMPLETE_TOKEN);
                                 logger.info("End");
 
                             } else {
-                                logger.info("Malformed message recived: " + line);
+                                logger.info("Malformed message received: " + line);
                             }
                         }
                     } catch (Exception e) {
