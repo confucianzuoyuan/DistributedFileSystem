99353b1c235637dee087260cc5d50aaaac2a018a
confucianzuoyuan <zuoyuantc@126.com>
2024-05-14 17:34:55 +0800

    update

--- Dstore.java
+++ Dstore.java
@@ -5,12 +5,8 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public class Dstore {
-
-    private static final Logger logger = Logger.getLogger(Dstore.class.getName());
-
     final int port;
     final int cport;
     int timeout;
@@ -44,73 +40,66 @@ public class Dstore {
                 f.delete();
             }
         } catch (Exception e) {
-            logger.info("error " + e.getMessage());
+            e.printStackTrace();
         }
 
         new Thread(this::ServerComms).start();
 
         try {
-            ServerSocket ss = new ServerSocket(port);
-            for (; ; ) {
+            var serverSocket = new ServerSocket(port);
+            while (true){
                 try {
-                    final Socket client = ss.accept();
+                    var client = serverSocket.accept();
                     client.setSoTimeout(timeout);
-                    logger.info("New connection");
-                    new Thread(new Runnable() {
-                        public void run() {
-                            try {
-                                BufferedReader in = new BufferedReader(
-                                        new InputStreamReader(client.getInputStream()));
-                                String line;
-                                logger.info("msg");
-                                while ((line = in.readLine()) != null) {
-                                    logger.info(line + " received");
-                                    String[] splitIn = line.split(" ");
-                                    String command = splitIn[0];
-                                    switch (command) {
-                                        case Protocol.STORE_TOKEN:
-                                        case Protocol.REBALANCE_STORE_TOKEN:
-                                            String fileName = splitIn[1];
-                                            int size = Integer.parseInt(splitIn[2]);
-                                            byte[] fileBuffer = new byte[size];
-                                            int buflen;
-                                            File outputFile = new File(dir, fileName);
-                                            FileOutputStream out = new FileOutputStream(outputFile);
-                                            InputStream fileInStream = client.getInputStream();
-                                            logger.info("filename: " + fileName);
-                                            sendMsg(client, Protocol.ACK_TOKEN);
-                                            while ((buflen = fileInStream.read(fileBuffer)) != -1) {
-                                                out.write(fileBuffer, 0, buflen);
-                                            }
-                                            if (command.equals(Protocol.STORE_TOKEN)) {
-                                                sendMsg(toServer, Protocol.STORE_ACK_TOKEN + " " + fileName);
-                                            }
-                                            filesStored.add(fileName);
-                                            fileSizes.put(fileName, size);
-                                            fileInStream.close();
-                                            out.close();
-                                            break;
-                                        case Protocol.LOAD_DATA_TOKEN:
-                                            if (!filesStored.contains(splitIn[1])) {
-                                                client.close();
-                                            }
-                                            sendFile(client, splitIn[1]);
-                                            break;
-                                        default:
-                                            logger.info("Malformed message recived: " + line);
-                                    }
+                    new Thread(() -> {
+                        try {
+                            var in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            String line;
+                            while ((line = in.readLine()) != null) {
+                                var words = line.split(" ");
+                                String command = words[0];
+                                switch (command) {
+                                    case Protocol.STORE_TOKEN:
+                                    case Protocol.REBALANCE_STORE_TOKEN:
+                                        var fileName = words[1];
+                                        int size = Integer.parseInt(words[2]);
+                                        byte[] fileBuffer = new byte[size];
+                                        int buflen;
+                                        File outputFile = new File(dir, fileName);
+                                        FileOutputStream out = new FileOutputStream(outputFile);
+                                        InputStream fileInStream = client.getInputStream();
+                                        sendMsg(client, Protocol.ACK_TOKEN);
+                                        while ((buflen = fileInStream.read(fileBuffer)) != -1) {
+                                            out.write(fileBuffer, 0, buflen);
+                                        }
+                                        if (command.equals(Protocol.STORE_TOKEN)) {
+                                            sendMsg(toServer, Protocol.STORE_ACK_TOKEN + " " + fileName);
+                                        }
+                                        filesStored.add(fileName);
+                                        fileSizes.put(fileName, size);
+                                        fileInStream.close();
+                                        out.close();
+                                        break;
+                                    case Protocol.LOAD_DATA_TOKEN:
+                                        if (!filesStored.contains(words[1])) {
+                                            client.close();
+                                        }
+                                        sendFile(client, words[1]);
+                                        break;
+                                    default:
+                                        System.out.println("Malformed message received: " + line);
                                 }
-                            } catch (Exception e) {
-                                logger.info("Exception caught " + e.getMessage());
                             }
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                     }).start();
                 } catch (Exception e) {
-                    logger.info("Exception caught " + e.getMessage());
+                    e.printStackTrace();
                 }
             }
         } catch (Exception e) {
-            logger.info("Exception caught " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -122,47 +111,43 @@ public class Dstore {
 
             // Receiving
             try {
-                for (; ; ) {
+                while (true){
                     try {
-                        BufferedReader in = new BufferedReader(
-                                new InputStreamReader(toServer.getInputStream()));
+                        var in = new BufferedReader(new InputStreamReader(toServer.getInputStream()));
                         String line;
                         while ((line = in.readLine()) != null) {
-                            logger.info(line + " received");
-                            String[] splitIn = line.split(" ");
-                            if (splitIn[0].equals(Protocol.LIST_TOKEN)) {
+                            var words = line.split(" ");
+                            if (words[0].equals(Protocol.LIST_TOKEN)) {
                                 var msgToSend = new StringBuilder(Protocol.LIST_TOKEN);
                                 for (String file : filesStored) {
                                     msgToSend.append(" ").append(file);
                                 }
                                 sendMsg(toServer, msgToSend.toString());
                                 // REMOVE
-                            } else if (splitIn[0].equals(Protocol.REMOVE_TOKEN)) {
-                                String fileName = splitIn[1];
+                            } else if (words[0].equals(Protocol.REMOVE_TOKEN)) {
+                                String fileName = words[1];
                                 if (filesStored.contains(fileName)) {
                                     File toRemove = new File(dir, fileName);
 
                                     if (toRemove.delete()) {
                                         filesStored.remove(fileName);
-                                        logger.info("File " + fileName + " removed");
                                         sendMsg(toServer, Protocol.REMOVE_ACK_TOKEN + " " + fileName);
                                     }
                                 } else {
                                     sendMsg(toServer, Protocol.ERROR_FILE_DOES_NOT_EXIST_TOKEN + " " + fileName);
-                                    logger.info("File " + fileName + " is not stored");
                                 }
-                            } else if (splitIn[0].equals(Protocol.REBALANCE_TOKEN)) {
-                                int noOfFiles = Integer.parseInt(splitIn[1]);
+                            } else if (words[0].equals(Protocol.REBALANCE_TOKEN)) {
+                                int noOfFiles = Integer.parseInt(words[1]);
                                 Hashtable<String, ArrayList<Integer>> filesToSend = new Hashtable<>();
                                 int offset = 2;
                                 for (int c = 0; c < noOfFiles; c++) {
-                                    String fileName = splitIn[offset];
+                                    String fileName = words[offset];
                                     offset += 1;
-                                    int noOfStores = Integer.parseInt(splitIn[offset]);
+                                    int noOfStores = Integer.parseInt(words[offset]);
                                     ArrayList<Integer> dStores = new ArrayList<>();
                                     for (int i = 1; i <= noOfStores; i++) {
                                         offset += 1;
-                                        dStores.add(Integer.valueOf(splitIn[offset]));
+                                        dStores.add(Integer.valueOf(words[offset]));
                                     }
                                     offset += 1;
                                     filesToSend.put(fileName, dStores);
@@ -180,16 +165,14 @@ public class Dstore {
                                                     BufferedReader in2 = new BufferedReader(
                                                             new InputStreamReader(dSock.getInputStream()));
                                                     String line2;
-                                                    logger.info("msg");
                                                     while ((line2 = in2.readLine()) != null) {
-                                                        logger.info(line2 + " received");
                                                         String[] splitIn2 = line2.split(" ");
                                                         if (splitIn2[0].equals(Protocol.ACK_TOKEN)) {
                                                             wait.countDown();
                                                         }
                                                     }
                                                 } catch (Exception e) {
-                                                    logger.info("error: " + e.getMessage());
+                                                    e.printStackTrace();
                                                 }
                                             }
                                         }).start();
@@ -199,37 +182,34 @@ public class Dstore {
                                         dSock.close();
                                     }
                                 }
-                                int noToRemove = Integer.parseInt(splitIn[offset]);
+                                int noToRemove = Integer.parseInt(words[offset]);
                                 offset += 1;
                                 for (int c = 0; c < noToRemove; c++) {
-                                    if (filesStored.contains(splitIn[offset])) {
-                                        File toRemove = new File(dir, splitIn[offset]);
+                                    if (filesStored.contains(words[offset])) {
+                                        File toRemove = new File(dir, words[offset]);
 
                                         if (toRemove.delete()) {
-                                            filesStored.remove(splitIn[offset]);
-                                            logger.info("File " + splitIn[offset] + " removed");
+                                            filesStored.remove(words[offset]);
                                         }
                                     } else {
-                                        logger.info("File " + splitIn[offset] + " is not stored");
+                                        System.out.println("File " + words[offset] + " is not stored");
                                     }
                                     offset += 1;
                                 }
                                 sendMsg(toServer, Protocol.REMOVE_COMPLETE_TOKEN);
-                                logger.info("End");
-
                             } else {
-                                logger.info("Malformed message received: " + line);
+                                System.out.println("Malformed message received: " + line);
                             }
                         }
                     } catch (Exception e) {
-                        logger.info("error " + e);
+                        e.printStackTrace();
                     }
                 }
             } catch (Exception e) {
-                logger.info("error " + e);
+                e.printStackTrace();
             }
         } catch (Exception e) {
-            logger.info("error" + e);
+            e.printStackTrace();
         }
     }
 
@@ -237,9 +217,8 @@ public class Dstore {
         try {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(msg);
-            logger.info("TCP message " + msg + " sent");
         } catch (Exception e) {
-            logger.info("error" + e);
+            e.printStackTrace();
         }
     }
 
@@ -252,10 +231,9 @@ public class Dstore {
             byte[] fileContent = new byte[(int) inputFile.length()];
             inputStream.read(fileContent);
             dataOut.write(fileContent);
-            logger.info(fileName + " file sent");
             inputStream.close();
         } catch (Exception e) {
-            logger.info("error " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
